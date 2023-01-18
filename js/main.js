101680a32b95d0d5a05d75bddf98c365b87cc0cd
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-01-18 16:53:54 -0300

    Added event listeners for clear, del and = buttons

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 function add(x, y) {
     return x + y;
 }
+
 function substract(x, y) {
     return x - y;
 }
+
 function multiply(x, y) {
     return x * y;
 }
+
 function divide(x, y) {
     return x / y;
 }
+
 function operate(x, y, operator) {
     x=Number(x);
     y=Number(y)
@@ -26,14 +30,25 @@ function operate(x, y, operator) {
             return null;
     }
 }
+
 let previousOperation = document.querySelector(".previousOperation");
 let currentResult = document.querySelector(".currentResult");
 let button = document.querySelectorAll('button');
+const clearButton = document.getElementById('clear');
+const populateResult = document.getElementById('=');
+let result = "result"
+
 // console.log(button) // works
-
-
-
 //     // previousOperation.innerHTML= "testing prevOp"; // WORKS
 //     // currentResult.innerHTML= "testing currRes"; // WORKS
-     button.forEach(button => {button.addEventListener("click", () => previousOperation.innerHTML = button.value)})
 
+// function populateDisplay() {
+//     let input = button.value;
+//     previousOperation.innerHTML = input;
+// }
+
+button.forEach(button => {button.addEventListener("click", () => previousOperation.innerHTML = button.value)})
+
+clearButton.addEventListener("click", () => previousOperation.innerHTML = "")
+clearButton.addEventListener("click", () => currentResult.innerHTML = "")
+populateResult.addEventListener("click", () => currentResult.innerHTML = result) //works
